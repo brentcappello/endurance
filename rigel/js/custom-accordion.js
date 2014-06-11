a8e50599f587e3582c330d1d8c3aa0378ebb58c5
Brent Cappello <brentcappello@gmail.com>
2014-06-11 08:25:12 -0700

    remove dupe dir

--- rigel/js/custom-accordion.js
+++
@@ -1,2 +0,0 @@
-$j(".pp_accordion_close").accordion({ active: 1, collapsible: true, clearStyle: true });
-$j(".pp_accordion").accordion({ active: 0, collapsible: true, clearStyle: true });
--- rigel/lib/customize.lib.php
+++
@@ -1,7 +0,0 @@
-<?php
-function themegoods_customize_register( $wp_customize ) {
-	$wp_customize->remove_section('colors');
-	$wp_customize->remove_section('background_image');
-}
-add_action( 'customize_register', 'themegoods_customize_register' );
-?>
